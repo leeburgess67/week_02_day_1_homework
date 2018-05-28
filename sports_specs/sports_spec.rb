1b9dcad929ec782dfe4b08e22f2d1f1918dff03e
leeburgess67 <leeburgessmail@gmail.com>
2018-05-28 16:20:22 +0100

    ALl Extension tests passing

--- sports_specs/sports_spec.rb
+++ sports_specs/sports_spec.rb
@@ -7,28 +7,50 @@ class SportsTeamTest < MiniTest::Test
 
   def test_get_team_name()
       team = SportsTeam.new("Celtic", ["Gordon", "Boyata", "Brown", "Dembele", "Eduard"], "Brendan Rodgers")
-      assert_equal("Celtic", team.get_team_name())
+      assert_equal("Celtic", team.team_name())
 
   end
   #
   def test_get_players()
       team = SportsTeam.new("Celtic", ["Gordon", "Boyata", "Brown", "Dembele", "Eduard"], "Brendan Rodgers")
 
-      assert_equal(["Gordon", "Boyata", "Brown", "Dembele", "Eduard"], team.get_players())
+      assert_equal(["Gordon", "Boyata", "Brown", "Dembele", "Eduard"], team.players())
 
   end
 
   def test_get_coach()
       team = SportsTeam.new("Celtic", ["Gordon", "Boyata", "Brown", "Dembele", "Eduard"], "Brendan Rodgers")
-      assert_equal("Brendan Rodgers", team.get_coach)
+      assert_equal("Brendan Rodgers", team.coach)
     end
 
 
     def test_coach_name_can_update
         team = SportsTeam.new("Celtic", ["Gordon", "Boyata", "Brown", "Dembele", "Eduard"], "Brendan Rodgers")
         team.update_coach("Pep Guardiola")
-        assert_equal("Pep Guardiola", team.get_coach)
+        assert_equal("Pep Guardiola", team.coach)
       end
+
+      def test_add_new_player
+              team = SportsTeam.new("Celtic", ["Gordon", "Boyata", "Brown", "Dembele", "Eduard"], "Brendan Rodgers")
+              team.add_new_player("Rogic")
+              assert_equal(["Gordon", "Boyata", "Brown", "Dembele", "Eduard", "Rogic"], team.players)
+
+      end
+
+      def test_if_player_in_team
+        team = SportsTeam.new("Celtic", ["Gordon", "Boyata", "Brown", "Dembele", "Eduard"], "Brendan Rodgers")
+
+        assert_equal("Yes, he is in the team", team.check_player("Eduard"))
+      end
+
+
+      def test_if_won_or_lost
+        team = SportsTeam.new("Celtic", ["Gordon", "Boyata", "Brown", "Dembele", "Eduard"], "Brendan Rodgers")
+        team.win_or_lose("win")
+        assert_equal(3, team.points)
+      end
+
+
 
 
 
